c56c4fa62dc99d28ee8eb67bca93f3e4a27275b4
Manfred Schulenburg <msori58@gmx.net>
2021-10-04 11:19:39 +0200

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -69,3 +69,42 @@ if (process.platform === 'darwin') {
   menuTemplate.unshift({});
 }
 
+// see https://www.geeksforgeeks.org/save-files-in-electronjs/
+
+const fs = require('fs');
+// Importing dialog module using remote
+const dialog = electron.remote.dialog;
+  
+var save = document.getElementById('save');
+  
+save.addEventListener('click', (event) => {
+    // Resolves to a Promise<Object>
+    dialog.showSaveDialog({
+        title: 'Select the File Path to save',
+        defaultPath: path.join(__dirname, '../assets/sample.txt'),
+        // defaultPath: path.join(__dirname, '../assets/'),
+        buttonLabel: 'Save',
+        // Restricting the user to only Text Files.
+        filters: [
+            {
+                name: 'Text Files',
+                extensions: ['txt', 'docx']
+            }, ],
+        properties: []
+    }).then(file => {
+        // Stating whether dialog operation was cancelled or not.
+        console.log(file.canceled);
+        if (!file.canceled) {
+            console.log(file.filePath.toString());
+              
+            // Creating and Writing to the sample.txt file
+            fs.writeFile(file.filePath.toString(), 
+                         'This is a Sample File', function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+        }
+    }).catch(err => {
+        console.log(err)
+    });
+});
